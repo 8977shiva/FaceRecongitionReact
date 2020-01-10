c6ce9e73fd0f54f700be1ed22b892df41d3a905f
8977shiva <b.skreddy1997@gmail.com>
2020-01-10 12:02:09 -0800

    Clarifi  api added

--- facedetection/src/App.js
+++ facedetection/src/App.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import Navigation from './components/navigation/navigation.component'
-import Logo from './components/Logo/logo-component'
-import './App.css';
-import ImageForm from './components/ImageFrom/ImageForm.component';
-import Rank from './components/Rank/rank.component';
+import  React, {Component } from "react";
+import Clarifai from 'clarifai'
+import Navigation from "./components/navigation/navigation.component";
+import Logo from "./components/Logo/logo-component";
+import "./App.css";
+import ImageForm from "./components/ImageFrom/ImageForm.component";
+import Rank from "./components/Rank/rank.component";
 
 
-function App() {
-  return (
-    <div className="App">
-     <Navigation/>
-     <Logo/>
-     <Rank/>
-     <ImageForm/>
-    </div>
+const app = new Clarifai.App({
+  apiKey: '5480d8bb32fd431193e2b80f7634f0b1'
+ });
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+ onInputChange=(event)=>{
+    console.log(event.target.value)
+ }
+ onButtonClick=()=>{
+   console.log('click')
+   app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+      console.log('working')
+    },
+    function(err) {
+      // there was an error
+      console.log(err)
+    }
   );
+ }
+  
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageForm onInputChange={this.onInputChange} onButtonClick={this.onButtonClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- facedetection/src/components/ImageFrom/ImageForm.component.jsx
+++ facedetection/src/components/ImageFrom/ImageForm.component.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
-
-const ImageForm =()=>{
-    return(
-        <div>
-        
-     <p className='f3 center'>{'This face Detection  app lets to detect face in your picture'} </p>
-        <div className='center'>
-        <div  className='pa4 br3  shadow-1 center'>
-            <input className='f4 pa2 w-70 ' type='text'/>
-             <button className='w-30 grow f4 link ph3 bgbutton  '>Dectect</button>
+const ImageForm = ({onInputChange,onButtonClick}) => {
+  return (
+    <div>
+      <p className="f3 center">
+        {"This face Detection  app lets to detect face in your picture"}{" "}
+      </p>
+      <div className="center">
+        <div className="pa4 br3  shadow-1 center">
+          <input
+            className="f4 pa2 w-70 "
+            type="text"
+            onChange={onInputChange}
+          />
+          <button className="w-30 grow f4 link ph3 bgbutton  " onClick={onButtonClick} >Dectect</button>
         </div>
-        </div>
-        </div>
-    )
-}
-export default ImageForm 
+      </div>
+    </div>
+  );
+};
+export default ImageForm;
